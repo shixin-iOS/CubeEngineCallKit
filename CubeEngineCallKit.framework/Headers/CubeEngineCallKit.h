2353d3795863c43e52b19cd6ec93c7badaa0f7e3
shixin-iOS <44627446+shixin-iOS@users.noreply.github.com>
2019-04-17 17:56:20 +0800

    Add files via upload

---
+++ CubeEngineCallKit.framework/Headers/CubeEngineCallKit.h
@@ -0,0 +1,27 @@
+//
+//  CubeEngineCallKit.h
+//  CubeEngineCallKit
+//
+//  Created by jianchengpan on 2018/5/14.
+//  Copyright © 2018年 CubeTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CubeEngineCallKit.
+FOUNDATION_EXPORT double CubeEngineCallKitVersionNumber;
+
+//! Project version string for CubeEngineCallKit.
+FOUNDATION_EXPORT const unsigned char CubeEngineCallKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CubeEngineCallKit/PublicHeader.h>
+
+#pragma - mark dependence
+#import <CubeEngineMediaKit/CubeEngineMediaKit.h>
+
+
+
+#pragma - mark call service
+
+//model
+#import <CubeEngineCallKit/CubeCallSession.h>
